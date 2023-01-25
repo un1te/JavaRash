b613572f05379101e493bdab57729fb4fb6e726c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-01-25 20:40:10 +0200

    Initial commit

---
+++ src/main/java/L1/FilesCopy/Main.java
@@ -0,0 +1,50 @@
+package L1.FilesCopy;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+// copy only files from one dit to another
+public class Main {
+    static Path sourceDirectory = Paths.get("src/main/java/L1/FilesCopy/source");
+    static Path targetDirectory = Paths.get("src/main/java/L1/FilesCopy/target");
+
+
+    public static void main(String[] args) throws IOException {
+
+        copyFiles();
+        deleteFilesFromTarget();
+
+    }
+
+    public static void copyFiles() {
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(sourceDirectory)) {  // get all files and dirs
+            for (Path path : files) {                                                   // loop all entries in files list
+                if (Files.isRegularFile(path)) {                                        // check if entry is a file and delete it
+                    Path target = targetDirectory.resolve(path.getFileName());
+                    Files.copy(path, target);
+                    System.out.println("<" + path.getFileName() + "> copied from <"
+                            + path.getParent() + "> to <" + target.getParent() + ">");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteFilesFromTarget() {
+        // Delete all files from target dir
+        try (DirectoryStream<Path> files = Files.newDirectoryStream(targetDirectory)) {
+            for (Path path : files) {
+                Files.delete(path);
+                System.out.println("files deleted");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
